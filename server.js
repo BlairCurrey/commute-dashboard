302379e40c1a056ab548808547eb6e4d608825ac
Blair Currey <curreyb88@gmail.com>
2020-04-18 18:20:34 -0400

    changed port to choose from env var if it exists

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
